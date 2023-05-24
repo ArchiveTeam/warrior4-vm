13827586aa5c440500366d9bcecbcebb7f36bce2
Christopher Foo <chris.foo@gmail.com>
2023-05-24 16:50:21 -0400

    Add rm to watchtower run-once container

--- appliance/skeleton/usr/lib/warrior4-appliance/watchtower-run-once-create.sh
+++ appliance/skeleton/usr/lib/warrior4-appliance/watchtower-run-once-create.sh
@@ -2,5 +2,7 @@
 # Create a container so updates can be checked for on startup
 # Note that this container name does not contain the string "watchtower" to avoid inaccurate grep results.
 # Copied from https://github.com/ArchiveTeam/Ubuntu-Warrior/blob/develop/startup.sh
+# Because the main Watchtower will detect this instance and refuse to run,
+# we have --rm to delete this container after this runs.
 set -e
-docker create --name watch-once-tower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --run-once
+docker create --name watch-once-tower --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --run-once
