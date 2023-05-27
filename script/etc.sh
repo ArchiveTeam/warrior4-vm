13f4649792a6461a29ee949c2390d2d490204430
Christopher Foo <chris.foo@gmail.com>
2023-05-27 14:47:39 -0400

    Bump version to beta

--- script/etc.sh
+++ script/etc.sh
@@ -2,7 +2,7 @@
 
 UID="$( head -c 8 /dev/urandom | xxd -p )"
 APP_NAME="archiveteam-warrior"
-APP_VERSION="4.0-alpha.1"
+APP_VERSION="4.0-beta.1"
 OUTPUT_DIR="output"
 QCOW2_DISK_FILENAME="warrior4-image.qcow2"
 VDI_DISK_FILENAME="warrior4-image.vdi"
